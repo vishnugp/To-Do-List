3cef6e4b507e206263cc7e8d07e2b8bcacb8f8a1
Vishnu G Prabhu <46211796+vishnugp@users.noreply.github.com>
2022-11-18 14:14:56 +0530

    Cleared errors in app.js

--- app.js
+++ app.js
@@ -141,7 +141,7 @@ app.get("/about", function(req,res){
 });
 
 let port = process.env.PORT;
-if(port= null || port = ""){
+if(port == null || port == ""){
   port = 3000;
 }
 app.listen(port,function(){
